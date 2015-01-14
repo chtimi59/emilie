ecba930d63bdf602c382d6a4fd8a14214cb5ff67
jan <jdorgeville@gmail.com>
2015-01-14 00:03:15 -0500

    remove track on linux/net

--- openwrt/build_dir/target-mipsel_74kc+dsp2_uClibc-0.9.33.2/linux-brcm47xx_mips74k/linux-3.14.27/drivers/net/wireless/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/net/wireless/built-in.o :=  mipsel-openwrt-linux-uclibc-ld  -m elf32ltsmip   -r -o drivers/net/wireless/built-in.o drivers/net/wireless/ti/built-in.o 
--- openwrt/build_dir/target-mipsel_74kc+dsp2_uClibc-0.9.33.2/linux-brcm47xx_mips74k/linux-3.14.27/drivers/net/wireless/ti/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/net/wireless/ti/built-in.o :=  rm -f drivers/net/wireless/ti/built-in.o; mipsel-openwrt-linux-uclibc-ar rcsD drivers/net/wireless/ti/built-in.o
