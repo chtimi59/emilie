675fbb1adfd147523d635db84e3caf187d422e1e
Jan dorgeville <jdorgeville@innobec.com>
2015-01-16 19:08:36 -0500

    hello world + image

---
+++ openwrt/extra/bcm4717-rules.mk
@@ -0,0 +1,42 @@
+#----------------------------------------
+#
+# COMMON RULES DEFINITIONS
+#
+#-------------------------------------
+
+.PHONY: install uninstall cpfiles clobber root.squashfs
+
+install:
+	$(info Copying files to $(ROOTFSDIR))
+	@$(STRIP_R) $(OUTDIR)
+	@cp -r $(OUTDIR)/* $(ROOTFSDIR)
+
+uninstall:
+	$(info Remove files in $(ROOTFSDIR))
+	@if [ -e $(OUTDIR_ESC) ]; then find $(OUTDIR_ESC) -type f | sed 's/$(OUTDIR_ESC)/$(ROOTFSDIR_ESC)/' | xargs rm -f; fi
+
+cpfiles:
+	@mkdir -p $(OUTDIR)
+	@cp -rf ./files/* $(OUTDIR)
+
+root.squashfs:
+	$(MKSQUASHFS) $(ROOTFSDIR) root.squashfs -nopad -noappend -root-owned -comp xz -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2 -b 256k \
+             -p '/dev d 755 0 0' -p '/dev/console c 600 0 0 5 1' -processors 1
+
+image: e2000.bin
+e2000.bin: root.squashfs $(LINUX_BUILD_DIR)/loader.gz $(LINUX_BUILD_DIR)/fs_mark
+	$(MKTRX) -o e2000.trx \
+             -f $(LINUX_BUILD_DIR)/loader.gz \
+             -f $(LINUX_BUILD_DIR)/vmlinux.lzma -a 1024 \
+             -f root.squashfs  -a 0x10000 \
+             -A $(LINUX_BUILD_DIR)/fs_mark
+	$(ADDPATTERN) -4 -p 32XN -v v1.0.4 -i e2000.trx -o e2000.bin 
+	@rm -f e2000.trx
+
+clobber: clean uninstall
+	@rm -f e2000.bin
+	@rm -f root.squashfs
+
+%.o: %.c
+	$(CC) -o $@ -c $< $(CFLAGS)
+
---
+++ openwrt/extra/bcm4717-toolchain.mk
@@ -0,0 +1,46 @@
+#----------------------------------------
+#
+# TOOLCHAIN CROSS-PLATFORM DEFINITION
+#
+#----------------------------------------
+
+export STAGING_DIR=$(OPENWRT)/staging_dir
+
+TOOLCHAIN=$(STAGING_DIR)/toolchain-mipsel_74kc+dsp2_gcc-4.8-linaro_uClibc-0.9.33.2
+TARGET=$(STAGING_DIR)/target-mipsel_74kc+dsp2_uClibc-0.9.33.2/
+SCRIPTS=$(OPENWRT)/scripts
+
+CROSS=$(TOOLCHAIN)/bin/mipsel-openwrt-linux-uclibc-
+export AR=$(CROSS)gcc-ar
+export AS=$(CROSS)gcc -c -Os -pipe -mno-branch-likely -mips32r2 -mtune=74kc -mdspr2 -fno-caller-saves -fhonour-copts -Wno-error=unused-but-set-variable -msoft-float
+export LD=$(CROSS)ld
+export NM=$(CROSS)gcc-nm
+export CC=$(CROSS)gcc
+export GCC=$(CROSS)gcc
+export CXX=$(CROSS)g++
+export RANLIB=$(CROSS)gcc-ranlib
+export STRIP=$(CROSS)strip
+export OBJCOPY=$(CROSS)objcopy
+export OBJDUMP=$(CROSS)objdump
+export SIZE=$(CROSS)size
+export STRIP_KMOD=$(SCRIPTS)/strip-kmod.sh
+export STRIP_R=$(SCRIPTS)/rstrip.sh
+export IPKG_BUILD=$(STAGING_DIR)/host/bin/ipkg-build -c -o 0 -g 0 
+export MKSQUASHFS=$(STAGING_DIR)/host/bin/mksquashfs4
+export MKTRX=$(STAGING_DIR)/host/bin/trx
+export ADDPATTERN=$(STAGING_DIR)/host/bin/addpattern
+
+export CFLAGS=-Os -pipe -mno-branch-likely -mips32r2 -mtune=74kc -mdspr2 -fno-caller-saves -fhonour-copts -Wno-error=unused-but-set-variable -msoft-float -mips16 -minterlink-mips16 
+export CXXFLAGS=-Os -pipe -mno-branch-likely -mips32r2 -mtune=74kc -mdspr2 -fno-caller-saves -fhonour-copts -Wno-error=unused-but-set-variable -msoft-float -mips16 -minterlink-mips16 
+export CPPFLAGS=-I$(TARGET)/usr/include -I$(TARGET)/include -I$(TOOLCHAIN)/usr/include -I$(TOOLCHAIN)/include 
+export LDFLAGS=-L$(TARGET)/usr/lib -L$(TARGET)/lib -L$(TOOLCHAIN)/usr/lib -L$(TOOLCHAIN)/lib 
+
+#COMMON FOLDERS
+export OUTDIR=out
+export OUTDIR_ESC=$(shell echo $(OUTDIR) | sed 's/\//\\\//g')
+export ROOTFSDIR=$(OPENWRT)/build_dir/target-mipsel_74kc+dsp2_uClibc-0.9.33.2/root-brcm47xx
+export ROOTFSDIR_ESC=$(shell echo $(ROOTFSDIR) | sed 's/\//\\\//g')
+export LINUXDIR=$(OPENWRT)/build_dir/target-mipsel_74kc+dsp2_uClibc-0.9.33.2/linux-brcm47xx_mips74k/
+export LINUX_BUILD_DIR=$(OPENWRT)/build_dir/target-mipsel_74kc+dsp2_uClibc-0.9.33.2/linux-brcm47xx_mips74k/
+
+
