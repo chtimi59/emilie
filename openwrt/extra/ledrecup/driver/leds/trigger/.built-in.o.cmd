0171d1f3cf9f26ffa90c0c09c03ed99a96287564
jan-dorgeville <jdorgeville@innobec.com>
2015-01-21 09:57:16 -0500

    add cef bootload document

--- openwrt/extra/ledrecup/driver/leds/trigger/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/leds/trigger/built-in.o :=  mipsel-openwrt-linux-uclibc-ld  -m elf32ltsmip   -r -o drivers/leds/trigger/built-in.o drivers/leds/trigger/ledtrig-timer.o drivers/leds/trigger/ledtrig-default-on.o 
